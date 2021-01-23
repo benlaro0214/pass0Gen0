3f23e514640779ebfcc71488878b6b692a192211
Ben Larocque <benlaro0214@hotmail.com>
2021-01-22 22:06:17 -0500

    turned character sets into arrays

--- Assets/script.js
+++ Assets/script.js
@@ -1,31 +1,46 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// from the idea file
+//var userOptions = {
+  //expectSpecialChars: false,
+  //expectNumericChars: false,
+  //expectUppercaseChars: false,
+  //expectLowercaseChars: false,}
+
+function generatePassword(){
+  var password = "";
+  var number = parseInt(prompt("Please Enter a number between 8-128 Characters:"));
+  if (number == null || number == "") { 
+  generatePassword()
+  } else {
+  var question1 = confirm ("Click Ok for lower Characters");
+  var question2 = confirm ("Click Ok for Upper Case Characters");
+  var question3 = confirm ("Click Ok for Numbers Characters");
+  var question4 = confirm ("Click Ok for Special Characters");
+  
+  for( var i=0; i<number; i++ ){
+    var charPick = Math.floor(Math.random()*charsLower.length)
+    password += charsLower[charPick]
+ 
+  }
+  return password
+    
+ 
+
+
+}
+
+}
 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-       passwordText.value = password;
+      passwordText.value = password;
 
 }
-
-
-//make values for the prompts and then assign those values to an Object. Use every proprety of it to let the fucntion whioch one of the character you want to use.
-// Use true of false since they are prompt boxes. https://www.w3schools.com/js/js_if_else.asp
-
-function generatePassword(){
-  var txt;
-  var number = prompt("Please Enter a number between 8-128 Characters:", "8");
-  if (number == null || number == "") {
-    txt = "User cancelled the prompt.";
-  } else {
-    (confirm("Click Ok for lower Characters"));
-    
-  }
-    
-  }
 
 
 // Add event listener to generate button
@@ -33,24 +48,14 @@ generateBtn.addEventListener("click", writePassword);
 
 
 // character sets
-var charsLower = "abcdefghijklmnopqrstuvwxyz"
-var charsUpper = charsLower.toUpperCase()
-var charsNumbers = "012345679"
-var charsSpecial = "!@#$%^&*()_+"
-// TODO from user prompt
-var userLength = 8
-// TODO userLower/userUpper/userNums/userSpecial from confirm boxes.
-var userLower = userUpper = userNums = userSpecial = true
-// build the random picker string based on user prompts
-var charSet =( userLower ? charsLower : '' )
-            +( userUpper ? charsUpper : '' )
-            +( userNums  ? charsNumbers : '' )
-            +( userSpecial ? charsSpecial : '' )
-// generate the string
-var password = ""
-for( var i=0; i<userLength; i++ ){
-    var charPick = Math.floor(Math.random()*charSet.length)
-    password += charSet[charPick]
-}
-// now plug this into the DOM
-document.querySelector(".answer").value = password
+var charsLower = Array.from ("abcdefghijklmnopqrstuvwxyz") 
+var chars = "abcdefghijklmnopqrstuvwxyz"
+var charsUpper = Array.from (chars.toUpperCase())
+var charsNumbers = Array.from ("012345679")
+var charsSpecial = Array.from ("!@#$%^&*()_+")
+
+
+
+
+
+
